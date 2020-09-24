8adbc4a8ad6e4aa87f5734d5ff5febb6ddc20290
Christian <botek.b1991@gmail.com>
2020-09-24 17:42:37 -0300

    hasta el ejercicio 5 inlusive

--- src/front/ts/main.ts
+++ src/front/ts/main.ts
@@ -6,6 +6,91 @@
  * Brief: Main frontend file (where the logic is)
 =============================================================================*/
 
+
+class Main {
+    main():void{
+       console.log("estoy en main()");
+       let usuarios:Array<User>;
+       usuarios = new Array<User>();
+       usuarios.push (new User(1,"Agustin","agustin@hotmail.com"));
+       usuarios.push (new User(2,"Brian","Brian@hotmail.com"));
+       usuarios.push (new User(3,"Santiago","Santiango@hotmail.com"));
+       let myframework=new MyFramework();
+       // boton va a tener almacenado la direccion del elemento html con el id "boton"
+       let boton=myframework.getElementById("boton");
+       boton.textContent="Acepto";
+       boton.addEventListener("click", this.evento);
+       this.mostrarUsers(usuarios);
+       for(let i in usuarios){
+           usuarios[i].printInfo();
+       }
+       
+    }
+
+    //Metodo ejercicio 4-2.3
+    mostrarUsers(usuarios:Array<User>):void{
+        for(let i in usuarios){
+            usuarios[i].printInfo();
+        }
+    }
+ 
+    //Ejercicio 5.1
+    evento(ev:Event):void {
+        console.log("se hizo click!");
+        console.log(this);
+    }
+
+}
+//Ejecicio 4
+class MyFramework {
+    getElementById(id:string):HTMLElement{
+        return document.getElementById(id);
+    }
+
+}
+
+class User {
+    private _id:number;
+    private _name:string;
+    private _email:string;
+    private _isLogged:boolean;
+
+    constructor(id:number, name:string, email:string){
+            this._id = id;
+            this._name = name;
+            this._email = email;
+    }
+
+set id(id:number){
+    this._id = id;
+}
+get id():number{
+    return this._id;
+}
+
+set name(name:string){
+    this._name = name;
+}
+get name():string{
+    return this._name;
+}
+
+printInfo():void{
+console.log("id ="+ this._id + ". name = " + this._name + ". email = " + this._email); 
+}
+}
+
+
+window.onload = function () {
+    let m:Main = new Main();
+    m.main();
+
+}
+
+
+
+
+
 //=======[ Settings, Imports & Data ]==========================================
 
 let user = "TypesScript Users!";
@@ -16,6 +101,7 @@ function greeter(person) {
     return "Hello, " + person;
  }
  
- document.body.innerHTML = greeter(user);
+ //document.body.innerHTML = greeter(user);
+ console.log("Hola mundo");
 
 //=======[ End of file ]=======================================================
